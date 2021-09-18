02af6a6f288d0d30ff8bb2dce8986c1ccf2e1117
nandan chauhan <rcrahul60@gmail.com>
2021-09-18 23:44:57 +0530

    initial author changes

---
+++ routes/authors.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const route = express.Router()
+const Author = require('../models/author')
+
+//all author routes
+
+route.get('/', async (req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+        // console.log(searchOptions)
+    }
+    try {
+        const authors = await Author.find(searchOptions)
+        //console.log(authors)
+        res.render('authors/index', { authors: authors, searchOptions: req.query })
+    } catch {
+        res.redirect('/')
+    }
+})
+
+
+
+route.get('/add', (req, res) => {
+    res.render('authors/add', { author: new Author() })
+})
+
+
+//create author route
+route.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+
+    })
+    try {
+        const newAuthor = await author.save()
+        res.redirect('authors')
+    } catch {
+        res.render('authors/add', {
+            author: author,
+            errorMessage: 'Error creating Author'
+        })
+    }
+
+
+
+    // author.save((err, newAuthor) => {
+    //     if (err) {
+    //         res.render('authors/add', {
+    //             author: author,
+    //             errorMessage: 'Error creating Author'
+    //         })
+    //     }
+    //     else {
+    //         res.redirect('authors')
+    //     }
+    // })
+    //res.send(req.body.name)
+})
+
+module.exports = route
